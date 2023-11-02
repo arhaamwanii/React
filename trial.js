d731dd7d15e8e8015b62b0f4cf98f27b9c1d77d0
arhaamwanii <arhamwani765@gmail.com>
2023-11-03 02:01:43 +0530

    .

--- trial.js
+++ trial.js
@@ -1,1 +1,18 @@
-const 
+class Animal{
+    constructor(type , legs){
+        this.type  = type;
+        this.legs = legs;
+    }
+}
+
+function yourName(name){
+
+    let legs = "anything" ;
+
+    this.legs = name;
+    console.log(this.legs)
+    
+    console.log(legs)
+}
+
+yourName("Arham Amin Wani");
