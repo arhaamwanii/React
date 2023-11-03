0ad8738d786d22d87b3de5f11503ec665c98b9ba
arhaamwanii <arhamwani765@gmail.com>
2023-11-03 20:35:52 +0530

    .

--- trial.js
+++ trial.js
@@ -37,40 +37,40 @@
 
 //FACTORY 
 
-function fst(radius){
-    return {
+// function fst(radius){
+//     return {
 
-        //ket value pairs - PROPERTIES
+//         //ket value pairs - PROPERTIES
         
-            pp: radius =  "2442",
+//             pp: radius =  "2442",
         
-        //FUNCTION - METHOD(logic)
+//         //FUNCTION - METHOD(logic)
         
-            draw: function(){
-                console.log(radius);
-            },
+//             draw: function(){
+//                 console.log(radius);
+//             },
 
-            one: function() {console.log(radius)}
-        };
+//             one: function() {console.log(radius)}
+//         };
         
-}
+// }
 
-const prduct1 =  fst(144);
+// const prduct1 =  fst(144);
 
-prduct1.draw();
+// prduct1.draw();
 
-console.log(prduct1)
+// console.log(prduct1)
 
 
-//we created a function/factory and we named it fst -- we put in the a parameter which we are going to use to diffenrntite between the differnt objects this factory is going to return us
+// //we created a function/factory and we named it fst -- we put in the a parameter which we are going to use to diffenrntite between the differnt objects this factory is going to return us
 //inside there wer put in a property and an method, 
     //in the property we redfined the value of radius, which otheerwise we had defined when calling the function
     //in the function we put a loggig the value of radius on the cosole command
 //then we called this ffactory using paramerters and then stored the value in const "product1",
 //what it basically did is that created a new object and stored that value in product1 - with the parameter provided while calling the function 
 //now we can acess that object using the "prduct1" keyword and can acess its properties and method like this
-    //product1.property
-    //
+    //product1.propertyName         
+    //product1.methodName()
 
 
 
@@ -79,4 +79,10 @@ console.log(prduct1)
 //so we use FACTORIES AND CONSTRUCTORS TO predeine repaitable stuff
 
 
+//CONSTRUCTOR
 
+class Animal{
+    constructor(one  , two){
+        
+    }
+}
