eae6435d2dfd3215b4fae62eee831a3b5cdab364
arhaamwanii <arhamwani765@gmail.com>
2023-11-03 19:41:00 +0530

    .

--- trial.js
+++ trial.js
@@ -18,18 +18,47 @@
 // yourName("Arham Amin Wani");
 
 
- let myDetails = {
-    name: "Arham",
-    residency: "Dalgate",
-    age: "17 years and 3 months",
-    timeIn: 18,
-    laverage: 3,
-    outPut: function(){
-        return this.timeIn * this.laverage;
-    }
- };
+//  let myDetails = {
+//     name: "Arham",
+//     residency: "Dalgate",
+//     age: "17 years and 3 months",
+//     timeIn: 18,
+//     laverage: 3,
+//     outPut: function(){
+//         return this.timeIn * this.laverage;
+//     }
+//  };
 
 
- let out = myDetails.outPut(45 , 11000);
+//  let out = myDetails.outPut(45 , 11000);
  
- console.log(out);
+//  console.log(out);
+
+
+//FACTORY 
+
+function factory(radius){
+    return {
+
+        //ket value pairs - PROPERTIES
+        
+            radius: radius,
+        
+        //FUNCTION - METHOD(logic)
+        
+            draw: function(){
+                console.log(radius);
+            }
+        };
+        
+}
+
+const prduct1 = factory(12);
+
+
+
+//DUPLUCATING OBJECT - can cause errors if the there are a lot of functions inside the obcject  
+//so we use FACTORIES AND CONSTRUCTORS TO predeine repaitable stuff
+
+
+
