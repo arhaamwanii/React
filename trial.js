ea944bb5602feba2b7922619f06a031be58039c3
arhaamwanii <arhamwani765@gmail.com>
2023-11-03 19:09:35 +0530

    .

--- trial.js
+++ trial.js
@@ -1,18 +1,35 @@
-class Animal{
-    constructor(type , legs){
-        this.type  = type;
-        this.legs = legs;
-    }
-}
+// class Animal{
+//     constructor(type , legs){
+//         this.type  = type;
+//         this.legs = legs;
+//     }
+// }
 
-function yourName(name){
+// function yourName(name){
 
-    let legs = "anything" ;
+//     let legs = "anything" ;
 
-    this.legs = name;
-    console.log(this.legs)
+//     this.legs = name;
+//     console.log(this.legs)
     
-    console.log(legs)
-}
+//     console.log(legs)
+// }
 
-yourName("Arham Amin Wani");
+// yourName("Arham Amin Wani");
+
+
+ let myDetails = {
+    name: "Arham",
+    residency: "Dalgate",
+    age: "17 years and 3 months",
+    timeIn: 18,
+    laverage: 3,
+    outPut: function(){
+        return this.timeIn * this.laverage;
+    }
+ };
+
+
+ let out = myDetails.outPut(45 , 11000);
+ 
+ console.log(out);
