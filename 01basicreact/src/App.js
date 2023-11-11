5f122d9ad1ebc4317dbf1c5a82f9a437bfdd0057
arhaamwanii <arhamwani765@gmail.com>
2023-11-11 07:14:20 +0530

    .

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -4,14 +4,10 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>they dont know me son </h1>
-      <p>in JSX it is necessary to wrap the inner elements in the outer element as it is the part of the syantax</p> 
-      <p> we learned the function of most of the files in the react folder</p>
-      <p>it is harder/takes more time to make the project eith normal npx</p>
-      <p>we can use vite and use its scripts to run the code, its like 5x lighter to make the projec using vite</p>
-      <p>vite dosent have node modules by defualt, we can install them in the folder "using npm istall" this shit will install all of the necessary modules for the build</p>
+    <h1>| They Don't Know me Son yup yup  | </h1>
     </>
   );
 }
-
+//app is just a function which returns HTML 
 export default App;
+//EXPORT THIS SHIT
--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+
+//core foundation library
+
 import ReactDOM from 'react-dom/client';
+
+//it is the implimentation of react on web 
+//just like react native is the implimentation of the react on thr mobile
+
+
+//DOM - TREE STRUCTURE , here we dont use the main dom i.e DOM of the website we make a Virtual DOM
+//it dosent actually make a completly new it just compares what is the main dom and manuplates it accodingly
+
 
 import App from './App';
 
+
+//createroot is a method  in the ReactDom
+//where => "document.getElementById('root')" this specifies the location
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +24,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+//we just rendered a fucntion whcih itself is rendering the HTML
+//react strict mode is not strictly to be used but it has some usecases during the devlopment
