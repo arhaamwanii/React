62d3a0b912cb0872a93de7ae6d1ae196568aa720
arhaamwanii <arhamwani765@gmail.com>
2023-11-12 21:29:17 +0530

    .

--- z-hooks-understand/src/App.jsx
+++ z-hooks-understand/src/App.jsx
@@ -5,25 +5,40 @@ import './App.css'
 
 function App() {
 
-  let counter = 15;
+  let [counter , setCounter ] = useState(15)
+
+  // let counter = 15;
 
   const addValue = () =>{
-    console.log("clicked" , counter)
-    counter = counter + 1;
     
+    // counter = counter + 1;
+    setCounter(counter + 2)
+    console.log("clicked" , counter);
   }
 
-  //so basically, the value of the counter is updating live but it is not being rendered real time this is where we need hooks to do this real time
+  const removeValue = () =>{
+    setCounter(counter - 3 )
+    console.log("clicked" + counter)
+  }
 
+  //so basically, the value of the counter is updating live but it is not being rendered real time this is where we need hooks to do this real time - REACTive
+  //controls the live updation of the UI|
+
+  //here we didnt even import js but this jsx still got converted this happened due to BABEL 
   return (
     <>
       <h1>React Hook</h1>
       <h2>Counter value : {counter}</h2>
 
-      <button onClick={addValue}>Add value</button>
+      <button onClick={addValue}>Add value to {counter}</button>
       <br />
       <br />
-      <button>Remove value</button>
+      <button onClick={removeValue}>Remove value to {counter}</button>
+
+      <footer>
+        <hr />
+        <h2>Current value {counter}</h2>
+      </footer>
     </>
   )
 }
