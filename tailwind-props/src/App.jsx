47214af9583211a0d9f3ddfe8ca55976d665b6b8
arhaamwanii <arhamwani765@gmail.com>
2023-11-13 00:13:35 +0530

    .

--- tailwind-props/src/App.jsx
+++ tailwind-props/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './card'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    <h1 className='bg-green-400 text-black rounded-xl '>TaiWind Check</h1>
+    <h1 className='mb-20'>Tail Wind Test</h1>
+    <Card/>
     </>
   )
 }
---
+++ tailwind-props/src/card.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./index.css"
+
+function Card(props) {
+
+    console.log("prop inside card = " + props) 
+
+  return (
+    <>
+<div className="relative h-[400px] w-[300px] rounded-md mb-10">
+  <img
+    src="https://images.pexels.com/photos/7372338/pexels-photo-7372338.jpeg"
+    alt="AirMax Pro"
+    className="z-0 h-full w-full rounded-md object-cover"
+  />
+  <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-left">
+    <h1 className="text-lg font-semibold text-white">Delba</h1>
+    <p className="mt-2 text-sm text-gray-300">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      debitis?
+    </p>
+    <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+      View Profile →
+    </button>
+  </div>
+</div>
+</>
+  )
+}
+
+export default Card;
