20cca3317ae4c5993fa6764f2607ce5c806b417b
arhaamwanii <arhamwani765@gmail.com>
2023-11-16 11:03:24 +0530

    .

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/header.jsx'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
      <h1 className='bg-green-600 p-4'>React Router</h1>
+    <Header/>
     </>
   )
 }
