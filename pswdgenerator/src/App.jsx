f5776175d42d81365c9b0f9585cf44f4461c79d7
arhaamwanii <arhamwani765@gmail.com>
2023-11-16 10:42:57 +0530

    .

--- pswdgenerator/src/App.jsx
+++ pswdgenerator/src/App.jsx
@@ -1,73 +1,99 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
+
+
 
 function App() {
- 
-  const [length , setLength] = useState(8);
-  // determines the change of the number when the limit bar is moved
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
 
-  const[numberAllow, setNUmberAllow] = useState(false);
-  //this determines that would there be a number in the random password
+  //useRef hook
+  const passwordRef = useRef(null)
 
-  const[charecterAllow, setCharecterAllow] = useState(false);
-  //this determines that would there be a number in the random password
-
-  const[pswd , setPswd] = useState("");
-  //liv 
-
-  const passwordGenertor = useCallback(() => {
+  const passwordGenerator = useCallback(() => {
     let pass = ""
-    let str = "ABCDEFGHIJKLMNOPQRSTVWXYZabcdefghijklmnopqrst"
-    if (numberAllowed) {
-      str += "0123456789"
-    }
-    if (charecterAllow) {
-      str += "!@#$%^&*"
-    }
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-    for (let i = 1; i <= i <= array.length; i++){
-        let char = Math.floor ( Math.random() * str.length + 1)
-        pass = str.charAt(char)
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+      
     }
 
-    setPswd(pass)
-
-  } , [length , charecterAllow , numberAllow, setPswd])
-  //when we call this we are going to generate a random password
+    setPassword(pass)
 
 
+  }, [length, numberAllowed, charAllowed, setPassword])
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
 
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
-    <>
-    <div className='w-full h-screen' style={{backgroundColor: "black" }}>
     
-
-    {/* to track the number - show it live changing on the screen will take USESTATE  */}
-
-
-    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800 '>
-      <div className='flex shadow rounded-lg overflow-hidden mb-4 flex-col '>
-
-        <h1 className='text-4xl text-center text-white pt-4' >Password Generator</h1>
-
-        <input type="text" value={pswd} className='outline-none w-full py-1 px-3'  placeholder='password' readOnly/>
-
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+        />
+        <button
+        onClick={copyPasswordToClipboard}
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+        
+    </div>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range"
+        min={6}
+        max={100}
+        value={length}
+         className='cursor-pointer'
+         onChange={(e) => {setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+      <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={() => {
+              setNumberAllowed((prev) => !prev);
+          }}
+      />
+      <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                  setCharAllowed((prev) => !prev )
+              }}
+          />
+          <label htmlFor="characterInput">Characters</label>
       </div>
     </div>
-
-
-    </div>
-    </>
+</div>
+    
   )
 }
 
 export default App
-
-//comment 1
-//comment 2
-//comment 3
-//comment 4 
-//comment 5
-//comment 6
-//comment 7 
-//comment 8
