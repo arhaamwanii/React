3903acc452f92a992543660ffb75660620376b96
arhaamwanii <arhamwani765@gmail.com>
2023-11-19 04:28:20 +0530

    .

--- reducer/src/App.jsx
+++ reducer/src/App.jsx
@@ -2,10 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [count , setCount] = useState(1);
+  const [showText , SetShowText] = useState(false);
 
   return (
-    <>
+    <> 
+    <h1>{count }</h1>
+
+    <button
+    onClick={
+      () => {
+        setCount(count * 9);
+        SetShowText(!showText);
+      }
+
+    }
+    >
+      click here
+    </button>
+
+    {showText || <p>Checking</p> }
+
 
     </>
   )
