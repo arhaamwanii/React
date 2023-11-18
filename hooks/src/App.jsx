5c57351ac51bfee57c6f96531c70bdcfb1bd6acd
arhaamwanii <arhamwani765@gmail.com>
2023-11-18 05:37:54 +0530

    .

---
+++ hooks/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const[counter , setCounter ] = useState(0);
+
+
+  //comman standard is using set before the name of the function and  after it naming it the state variable name
+
+  function SetCounter(){
+    setCounter(counter + 7)
+
+  }
+  return (
+    <>
+    <button onClick={SetCounter}>Add 7</button>
+    <h1>{counter}</h1>
+    </>
+  )
+}
+
+export default App
