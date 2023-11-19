d893f4782700bfe5ba1f567662a892943556f907
arhaamwanii <arhamwani765@gmail.com>
2023-11-19 06:54:11 +0530

    .

--- EFFECT/src/App.jsx
+++ EFFECT/src/App.jsx
@@ -1,16 +1,27 @@
 import { useEffect } from 'react'
 import './App.css'
 import axios from "axios";
+import { useState } from 'react';
 
 function App() {
 
+  const [arham , setArham ] = useState("");
+
+ 
   useEffect( () => {
-    console.log("intial render on page load")
+   axios.get("https://jsonplaceholder.typicode.com/comments").then((whatever) =>{
+    setArham(whatever.data[0].email)
+    console.log("number of times")
+   })
   })
 
+  //the message is logged more than once, i.e however many times the page is re-rendered the this command will be called again thus making it log every time it re-remders 
+
+  //whatever we put in the function is what will be called when we load the page initially - wheneever the page renders?!
+//this here will be called wheneve a state is canged so yead
   return (
     <>
-    <h1>Hello World</h1>
+    <h1>Hello, {arham}</h1>
     </> 
   )
 }
