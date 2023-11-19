6cc2f4178829bcee0aa6ba3b12313941d9d5b0e9
arhaamwanii <arhamwani765@gmail.com>
2023-11-19 07:06:30 +0530

    .

--- EFFECT/src/App.jsx
+++ EFFECT/src/App.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 function App() {
 
   const [arham , setArham ] = useState("");
+  const (count , setCount) = useState(0);
 
  
   useEffect( () => {
@@ -13,15 +14,26 @@ function App() {
     setArham(whatever.data[0].email)
     console.log("number of times")
    })
-  })
+  }, [])
+//allows you to pass all the states that we want to listen to, but in this case we pass an emply array which means it wont listen to anything - thus wont change its value 
+//If you pass an empty array ([]) as the second argument to the useEffect hook, it means that the effect will only run once when the component mounts, and it won't run again for subsequent renders
 
   //the message is logged more than once, i.e however many times the page is re-rendered the this command will be called again thus making it log every time it re-remders 
-
   //whatever we put in the function is what will be called when we load the page initially - wheneever the page renders?!
 //this here will be called wheneve a state is canged so yead
   return (
     <>
     <h1>Hello, {arham}</h1>
+    <h1>{count}</h1>
+    <button
+    onClick={
+      () => {
+        setCount(count + 1)
+      }
+    }
+    >
+      Add
+    </button>
     </> 
   )
 }
