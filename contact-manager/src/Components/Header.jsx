b2ea177752dcb1366ed155afea2fa66a0973efc9
arhaamwanii <arhamwani765@gmail.com>
2023-11-22 13:10:07 +0530

    .

---
+++ contact-manager/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const header = () => {
+  return (
+    <div className='ui fixed menu'>
+        <div className='ui container center '>
+        <h2>Arham</h2>
+        </div>
+    </div>
+  )
+}
